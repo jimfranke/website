a28e49ba768276ce732855c8434e5930a39e3921
Jim Franke <info@franke.app>
2020-12-28 13:37:22 +0100

    x

--- play/js/constants.js
+++ play/js/constants.js
@@ -1,5 +1,5 @@
 export const BOARD_COLS = 10;
 export const BOARD_ROWS = 20;
-export const BLOCK_SIZE = 30;
+export const BLOCK_SIZE = 25;
 export const NEXT_SIZE = 6;
 export const GHOST_OPACITY = 50;
