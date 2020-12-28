1799a1b3e9d6e485bfb605627c9a12aeaebcd774
Jim Franke <info@franke.app>
2020-12-28 14:39:24 +0100

    x

--- play/js/input.js
+++ play/js/input.js
@@ -117,7 +117,7 @@ export const handleGameInput = ({ store, render, $paused, $controls }) => {
     touchTimers.push(
       setInterval(() => {
         handleAction(action);
-      }, 50),
+      }, 60),
     );
   });
 
