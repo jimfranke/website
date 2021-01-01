575b9ed0cc214c0c6b9bbdd0b6717b765e8bda35
Jim Franke <info@franke.app>
2021-01-01 15:42:33 +0100

    x

--- tetris/js/app.js
+++ tetris/js/app.js
@@ -3,58 +3,56 @@ import { handleGameInput, handleMenuInput } from './input.js';
 import { createRenderer } from './render.js';
 import { store } from './store.js';
 
-export const app = () => {
-  const $app = document.getElementById('app');
-  const $menu = $app.querySelector('.menu');
-  const $game = $app.querySelector('.game');
-  const $paused = $game.querySelector('.view__paused');
-  const $controls = $game.querySelector('.controls');
+const $app = document.getElementById('app');
+const $menu = $app.querySelector('.menu');
+const $game = $app.querySelector('.game');
+const $paused = $game.querySelector('.view__paused');
+const $controls = $game.querySelector('.controls');
 
-  const $statsScore = $game.querySelector('[data-stats=score]');
-  const $statsLines = $game.querySelector('[data-stats=lines]');
-  const $statsLevel = $game.querySelector('[data-stats=level]');
+const $statsScore = $game.querySelector('[data-stats=score]');
+const $statsLines = $game.querySelector('[data-stats=lines]');
+const $statsLevel = $game.querySelector('[data-stats=level]');
 
-  const $mainCanvas = $game.querySelector('.view__canvas-main');
-  const mainContext = $mainCanvas.getContext('2d');
-  const $holdCanvas = $game.querySelector('.view__canvas-hold');
-  const holdContext = $holdCanvas.getContext('2d');
-  const $nextCanvas = $game.querySelector('.view__canvas-next');
-  const nextContext = $nextCanvas.getContext('2d');
+const $mainCanvas = $game.querySelector('.view__canvas-main');
+const mainContext = $mainCanvas.getContext('2d');
+const $holdCanvas = $game.querySelector('.view__canvas-hold');
+const holdContext = $holdCanvas.getContext('2d');
+const $nextCanvas = $game.querySelector('.view__canvas-next');
+const nextContext = $nextCanvas.getContext('2d');
 
-  $mainCanvas.width = BLOCK_SIZE * BOARD_COLS;
-  $mainCanvas.height = BLOCK_SIZE * BOARD_ROWS;
-  $nextCanvas.width = BLOCK_SIZE * 4;
-  $nextCanvas.height = BLOCK_SIZE * (NEXT_SIZE * 3 - 1);
-  $holdCanvas.width = BLOCK_SIZE * 4;
-  $holdCanvas.height = BLOCK_SIZE * 2;
+$mainCanvas.width = BLOCK_SIZE * BOARD_COLS;
+$mainCanvas.height = BLOCK_SIZE * BOARD_ROWS;
+$nextCanvas.width = BLOCK_SIZE * 4;
+$nextCanvas.height = BLOCK_SIZE * (NEXT_SIZE * 3 - 1);
+$holdCanvas.width = BLOCK_SIZE * 4;
+$holdCanvas.height = BLOCK_SIZE * 2;
 
-  const getSelectedLevel = () =>
-    parseInt($menu.querySelector('.menu__input-level-select').value);
+const getSelectedLevel = () =>
+  parseInt($menu.querySelector('.menu__input-level-select').value);
 
-  const render = createRenderer({
-    store,
-    mainContext,
-    nextContext,
-    holdContext,
-    updateStats: ({ score, lines, level }) => {
-      $statsScore.textContent = score;
-      $statsLines.textContent = lines;
-      $statsLevel.textContent = level;
-    },
-  });
+const render = createRenderer({
+  store,
+  mainContext,
+  nextContext,
+  holdContext,
+  updateStats: ({ score, lines, level }) => {
+    $statsScore.textContent = score;
+    $statsLines.textContent = lines;
+    $statsLevel.textContent = level;
+  },
+});
 
-  handleMenuInput({
-    store,
-    render,
-    getSelectedLevel,
-    $menu,
-    $game,
-  });
+handleMenuInput({
+  store,
+  render,
+  getSelectedLevel,
+  $menu,
+  $game,
+});
 
-  handleGameInput({
-    store,
-    render,
-    $paused,
-    $controls,
-  });
-};
+handleGameInput({
+  store,
+  render,
+  $paused,
+  $controls,
+});
