b95d4a3221d0c7dc97043115531cfe0242b8e3c2
Jim Franke <info@franke.app>
2021-01-07 09:17:09 +0100

    x

--- tetris/js/render.js
+++ tetris/js/render.js
@@ -38,7 +38,7 @@ export const createRenderer = ({
         inputQueue: inputQueue.slice(1),
       });
     }
-    const dropSpeed = DROP_SPEEDS[level] ?? fastestDropSpeed;
+    const dropSpeed = DROP_SPEEDS[level - 1] ?? fastestDropSpeed;
     if (!activeTetromino || activeTetromino.isLocked) {
       state = setState(
         ({ nextTetrominoQueue, activeTetromino } = shiftNextTetrominoQueue(
