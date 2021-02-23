8302355615d41fb10f4f12735348683b5c940bde
Jim Franke <info@franke.app>
2021-02-23 13:27:13 +0100

    x

--- tetra/js/app.js
+++ tetra/js/app.js
@@ -6,7 +6,6 @@ import {
 } from './constants.js';
 import { $holdCanvas, $mainCanvas, $nextCanvas } from './dom.js';
 import { handleInput } from './input.js';
-import { render } from './render.js';
 
 $mainCanvas.width = BLOCK_SIZE * BOARD_COLS;
 $mainCanvas.height = BLOCK_SIZE * BOARD_ROWS;
@@ -15,5 +14,4 @@ $nextCanvas.height = BLOCK_SIZE * (NEXT_QUEUE_SIZE * 3 - 1);
 $holdCanvas.width = BLOCK_SIZE * 4;
 $holdCanvas.height = BLOCK_SIZE * 2;
 
-render();
 handleInput();
